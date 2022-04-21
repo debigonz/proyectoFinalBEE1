cb42a46d544c5b23584179f35c26054bddf121b0
deborah.gonzalez <deborah.gonzalez@globant.com>
2022-04-21 16:33:16 -0300

    [MOD] catalog service and configuration repo

--- catalog-service/src/main/java/com/dh/catalogservice/domain/model/document/Catalog.java
+++ catalog-service/src/main/java/com/dh/catalogservice/domain/model/document/Catalog.java
@@ -2,12 +2,16 @@ package com.dh.catalogservice.domain.model.document;
 
 import com.dh.catalogservice.domain.model.dto.MovieWS;
 import com.dh.catalogservice.domain.model.dto.SerieWS;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Document(collection = "catalog")
 public class Catalog {
@@ -17,6 +21,5 @@ public class Catalog {
     private List<MovieWS> movies;
     private List<SerieWS> series;
 
-    public Catalog(String genre, List<MovieWS> movieWSList, List<SerieWS> serieWSList) {
-    }
+
 }
